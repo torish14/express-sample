d29b60a3eb12d98b2471262d5f7855faa6bdf09f
toshihirokato <nanamin.104@icloud.com>
2020-03-29 02:35:19 +0900

    ルーティングの実装

---
+++ router/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+// ルーティング用のオブジェクトを express.Router() で作成して変数に router に代入する
+const router = express.Router()
+
+// GET/, GET/about にリクエストがあった場合の処理を router に実装する
+router
+.get('/', (req, res) => {
+  res.send('トップページ！')
+})
+.get('/about', (req, res) => {
+  res.send('アバウトページ！')
+})
+
+// ルーティング処理の実装を行ったルーティングオブジェクトを外部に公開する
+modules.exports = router
