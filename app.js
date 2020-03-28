a5ef4da536fb40bdc488b13989da0d4f992eef70
toshihirokato <nanamin.104@icloud.com>
2020-03-29 02:14:18 +0900

    コメントメッセージの追記

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// express の使用宣言
 const express = require('express')
+// express() でexpressが提供している機能を利用できるオブジェクトを取得する
 const app = express()
 const PORT = 3000
 
+// app.get('/', コールバック関数)で、ルートに HTTP メソッドの GET を使ってリクエストが来たときの処理を実装する
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// app.listen(ポート番号、 コールバック関数)で、指定したポート番号でWebサーバを起動する
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 })
