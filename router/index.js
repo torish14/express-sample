811df0f1ba261ad5918a3be0563f88c4fba6a9dd
toshihirokato <nanamin.104@icloud.com>
2020-03-29 02:51:58 +0900

    module.exports ミスの修正

--- router/index.js
+++ router/index.js
@@ -4,12 +4,12 @@ const router = express.Router()
 
 // GET/, GET/about にリクエストがあった場合の処理を router に実装する
 router
-.get('/', (req, res) => {
-  res.send('トップページ！')
-})
-.get('/about', (req, res) => {
-  res.send('アバウトページ！')
-})
+  .get('/', (req, res) => {
+    res.send('トップページ！')
+  })
+  .get('/about', (req, res) => {
+    res.send('アバウトページ！')
+  })
 
 // ルーティング処理の実装を行ったルーティングオブジェクトを外部に公開する
-modules.exports = router
+module.exports = router
