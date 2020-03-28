6ff367b732c15ee989188fbc9a4b9e682ff59a58
toshihirokato <nanamin.104@icloud.com>
2020-03-29 04:13:19 +0900

    index ページ内で表示する要素

--- router/index.js
+++ router/index.js
@@ -6,7 +6,19 @@ const router = express.Router()
 router
   .get('/', (req, res) => {
     // テンプレートエンジンを用いる際は、res.send ではなく res.render を使う
-    res.render('index')
+    res.render('index', {
+      data: 'トップページで使うデータ',
+      siteInfo: {
+        name: 'Web白熱教室',
+        owner: 'つよぽん'
+      },
+      items: [
+        {name: 'アイテム1'},
+        {name: 'アイテム2'},
+        {name: 'アイテム3'},
+      ],
+      title: 'トップページのタイトル'
+    })
   })
   .get('/about', (req, res) => {
     res.render('about')
